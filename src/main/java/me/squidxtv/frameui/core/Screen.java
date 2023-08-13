45f6ce90f4a7cb32f4cca98326db85b7f73de907
SquidXTV <squidxtv@gmail.com>
2023-08-13 15:07:00 +0200

    Finished AbstractScreen and VirtualScreen

---
+++ src/main/java/me/squidxtv/frameui/core/Screen.java
@@ -0,0 +1,24 @@
+package me.squidxtv.frameui.core;
+
+import me.squidxtv.frameui.core.actions.Clickable;
+import me.squidxtv.frameui.core.actions.Scrollable;
+import me.squidxtv.frameui.core.content.ScreenModel;
+import me.squidxtv.frameui.core.graphics.Graphics;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+
+
+public interface Screen<G extends Graphics<?>> extends Clickable, Scrollable {
+
+    void open();
+    void close();
+    void remove();
+    void update();
+
+    @NotNull G getGraphics();
+    void setGraphics(@NotNull G graphics);
+
+    @NotNull JavaPlugin getPlugin();
+    @NotNull ScreenModel getModel();
+    void setModel(@NotNull ScreenModel model);
+}
