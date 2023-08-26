1838044473c92fea8ead9c083331d74c4027fcdc
Connor Schweighöfer <squidxtv@gmail.com>
2023-08-26 05:44:05 -0700

    Add clicking/scrolling to visual testing (#9)

---
+++ src/test/java/me/squidxtv/visual/ui/SwingScreenRenderer.java
@@ -0,0 +1,49 @@
+package me.squidxtv.visual.ui;
+
+import me.squidxtv.frameui.core.actions.initiator.ActionInitiator;
+import me.squidxtv.frameui.core.actions.scroll.Scrollable;
+import me.squidxtv.visual.screen.DebugScreen;
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class SwingScreenRenderer extends JLabel {
+
+    public SwingScreenRenderer(DebugScreen screen) {
+        setIcon(new ImageIcon(screen.getGraphics().getImage()));
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() != MouseEvent.BUTTON1) {
+                    return;
+                }
+                screen.click(ActionInitiator.ofClass(SwingScreenRenderer.class), e.getX(), e.getY());
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {}
+            @Override
+            public void mouseReleased(MouseEvent e) {}
+            @Override
+            public void mouseEntered(MouseEvent e) {}
+            @Override
+            public void mouseExited(MouseEvent e) {}
+
+        });
+
+        addMouseWheelListener(e -> {
+            Scrollable.ScrollDirection direction;
+            if (e.getWheelRotation() > 0) {
+                direction = Scrollable.ScrollDirection.LEFT;
+            } else if (e.getWheelRotation() < 0) {
+                direction = Scrollable.ScrollDirection.RIGHT;
+            } else {
+                throw new IllegalStateException("Illegal wheel rotation direction.");
+            }
+            screen.scroll(ActionInitiator.ofClass(SwingScreenRenderer.class), direction, e.getX(), e.getY());
+        });
+    }
+
+
+}
