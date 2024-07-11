ff8c3a6644a488c8ea96fb9cc4b7759bc13cf51e
JW <jacekwoln@gmail.com>
2024-07-11 10:28:29 +0200

    - Create class FramesApi as 'god object' and entry point of library. It is like "Bukkit"
    - Add Lombok dependency to reduce boilerplate
    
    - Pass Plugin instance to ScreenRegistry
    - Create ScreenProperties class. It should only
      hold serializable data so we can simply dump to Json or load it from Json
    
      - add more methods to ScreenBuilder

--- src/main/java/me/squidxtv/frameui/api/FramesApi.java
+++ src/main/java/me/squidxtv/frameui/api/FramesApi.java
@@ -5,28 +5,42 @@ import me.squidxtv.frameui.api.exceptions.FramesApiException;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.ServicesManager;
 
-/**
- * code```
- * <p>
- * var screen = FramesApi
- * .newScreen()
- * .withName("Default screen!")
- * .withSize(10, 10)
- * .build();
- * <p>
- * screen.open();
- * ```
- */
-public interface FramesApi {
 
+public interface FramesApi {
+    /**
+     * @return true if library has been initialed
+     */
     static boolean isInitialized() {
         return getServices().isProvidedFor(ScreenRegistry.class);
     }
 
+    /**
+     * Use this method to create new screen
+     * <p>
+     * code```
+     * <p>
+     * var screen = FramesApi
+     * .newScreen()
+     * .withName("Default screen!")
+     * .withSize(10, 10)
+     * .build();
+     * <p>
+     * screen.open();
+     * ```
+     *
+     * @return screen creator
+     * @see ScreenBuilder
+     */
     static ScreenBuilder newScreen() {
         return screens().newScreen();
     }
 
+
+    /**
+     * @return ScreenRegistry object that manage all screens
+     * @throws FramesApiException when library has not been initialized
+     * @see ScreenRegistry
+     */
     static ScreenRegistry screens() {
         if (!isInitialized()) {
             throw new FramesApiException("Frames Api has not been initialized yet!");
