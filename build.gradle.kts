e81ec501452f44c928a8affe9a4d979f456a4a01
Connor Schweighoefer <squidxtv@gmail.com>
2024-10-22 21:35:44 +0200

    Setup gradle staging artifacts and copyright headers

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 group = "me.squidxtv"
-version = "1.0-SNAPSHOT"
-description = "FrameUI"
+version = "1.0.0"
+description = "FrameUI is a Minecraft plugin library designed to easily create screens within a server."
 
 repositories {
     mavenCentral()
@@ -73,23 +73,79 @@ tasks {
 }
 
 spotless {
+    encoding("UTF-8")
     java {
         endWithNewline()
         removeUnusedImports()
 
         importOrder("", "me.squidxtv", "java", "\\#")
         eclipse().configFile("${rootProject.rootDir}/config/custom-eclipse-style.xml")
+
+        licenseHeader("""
+            /*
+             * FrameUI: Minecraft plugin library designed to easily create screens within a server.
+             * Copyright (C) 2023-${'$'}YEAR Connor Schweighöfer
+             *
+             * This program is free software: you can redistribute it and/or modify
+             * it under the terms of the GNU General Public License as published by
+             * the Free Software Foundation, either version 3 of the License, or
+             * (at your option) any later version.
+             *
+             * This program is distributed in the hope that it will be useful,
+             * but WITHOUT ANY WARRANTY; without even the implied warranty of
+             * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+             * GNU General Public License for more details.
+             *
+             * You should have received a copy of the GNU General Public License
+             * along with this program. If not, see <https://www.gnu.org/licenses/>.
+             */
+        """.trimIndent())
     }
 }
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            group = "me.squidxtv"
-            artifactId = "FrameUI"
-            version = version
+            group = rootProject.group
+            artifactId = rootProject.name
+            version = rootProject.version.toString()
 
             from(components["java"])
+
+            pom {
+                name = rootProject.name
+                description = project.description
+                url = "https://github.com/SquidXTV/FrameUI"
+
+                licenses {
+                    license {
+                        name = "GPL-3.0-or-later"
+                        url = "https://spdx.org/licenses/GPL-3.0-or-later.html"
+                    }
+                }
+
+                developers {
+                    developer {
+                        id = "squidxtv"
+                        name = "Connor Schweighöfer"
+                        email = "squidxtv@gmail.com"
+                        url = "https://squidxtv.me/"
+                        timezone = "Europe/Berlin"
+                    }
+                }
+
+                scm {
+                    connection = "scm:git:https://github.com/SquidXTV/FrameUI.git"
+                    developerConnection = "scm:git:ssh://github.com/SquidXTV/FrameUI.git"
+                    url = "https://github.com/SquidXTV/FrameUI.git"
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = uri(layout.buildDirectory.dir("staging-deploy"))
         }
     }
 }
--- src/main/java/me/squidxtv/frameui/FrameUI.java
+++ src/main/java/me/squidxtv/frameui/FrameUI.java
@@ -1,16 +1,34 @@
+/*
+ * FrameUI: Minecraft plugin library designed to easily create screens within a server.
+ * Copyright (C) 2023-2024 Connor Schweighöfer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
 package me.squidxtv.frameui;
 
-import me.squidxtv.frameui.api.ScreenRegistry;
-import me.squidxtv.frameui.api.ScreenRegistryImpl;
-import me.squidxtv.frameui.listener.ClickListener;
-import me.squidxtv.frameui.listener.LeaveListener;
-import me.squidxtv.frameui.listener.ScrollListener;
 import org.bukkit.Server;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.ServicePriority;
 import org.bukkit.plugin.ServicesManager;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import me.squidxtv.frameui.api.ScreenRegistry;
+import me.squidxtv.frameui.api.ScreenRegistryImpl;
+import me.squidxtv.frameui.listener.ClickListener;
+import me.squidxtv.frameui.listener.LeaveListener;
+import me.squidxtv.frameui.listener.ScrollListener;
 
 /**
  * The {@code FrameUI} class is the main plugin class for the FrameUI plugin
